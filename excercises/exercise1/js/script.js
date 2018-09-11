8ea6f28a63ed4df6ca8725d5f45f227963d7b820
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-09-11 17:19:58 -0400

    Created assets and loaded an image sucessfully

--- excercises/exercise1/js/script.js
+++ excercises/exercise1/js/script.js
@@ -1,21 +1,28 @@
 
+//shape1
 let shape1x; //starting xpos of shape1
 let shape1y; //starting ypos of shape 1
 const shape1r = 20; //radius of shape 1
 const shape1xinc = 1; //constant at which to increment shape 1's xpos
 
-function preload(){
+let img1;
 
+function preload(){
+let img1 = loadImage("assets/images/thing1.png");
 }
 
 function setup(){
   createCanvas(displayWidth,displayHeight); //create canvas with the same dimensions as the screen resolution;
   shape1x = 0 - shape1r; //starting xpos of shape 1 set to just left of the drawn canvas
   shape1y = height/2; //starting ypos of shape 1 set to middle of screen;
+
 }
 
 function draw(){
   background(51); //fill the screen with dark grey;
   shape1x += shape1xinc;
   ellipse(shape1x,shape1y,shape1r); //draw shape 1
+
+  image(img1,0,0,img.width/2,img.height/2);
+
 }
