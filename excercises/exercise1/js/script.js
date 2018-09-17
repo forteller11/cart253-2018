53b87f887ed7fc106f5d5f5a69bec187da90a485
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-09-17 15:13:52 -0400

    Added a shape whose position represented a sin function
    I created a sin function (sin(x)). I modified it so that its values would be mapped so that they would remain within the dimensions of the canvas while still taking up a sizeable portion of the screen (so you could actually see the representation of the function)
    To do this I multiplied sin(x) by the height of the (canvas/4), so the y-values of the function would take up exactly half of the canvas vertically, I then translated the function down by half a screen so the function was centered relative to the canvas. Then I stretched the function horizontally by a factor of 100 so that there would be relatively less waves (periods) within the canvas-space. Finally I iterrated through the xpositions once per frame by 1 and drew a sphere at the x and y point in the function.

--- excercises/exercise1/js/script.js
+++ excercises/exercise1/js/script.js
@@ -5,14 +5,23 @@ let shape1y; //starting ypos of shape 1
 const shape1r = 20; //radius of shape 1
 const shape1xinc = 1; //constant at which to increment shape 1's xpos
 
+//img 1
 let img1;
 const img1_scale = 0.2;
 
+//img 2
 let img2;
 const img2_scale = 0.6;
 const img2_followspeed = 10; //the higher the number the less quickly img2 will catch up to the mouse;
 let img2_x = 0;
 let img2_y = 0;
+
+//shape 3
+let shape3x; //starting xpos of shape3
+let shape3y; //starting ypos of shape 3
+const shape3r = 10; //radius of shape 3
+const shape3xinc = 1; //constant at which to increment shape 3's xpos
+
 function preload(){
 img1 = loadImage("assets/images/thing1.png");
 
@@ -20,12 +29,16 @@ img2 = loadImage("assets/images/thing2.png");
 }
 
 function setup(){
+  angleMode(RADIANS);
   createCanvas(displayWidth,displayHeight); //create canvas with the same dimensions as the screen resolution;
 
   //shape 1
   shape1x = 0 - shape1r; //starting xpos of shape 1 set to just left of the drawn canvas
   shape1y = height/2; //starting ypos of shape 1 set to middle of screen;
 
+  //shape 3
+  shape3x = 0 - shape1r;
+  shape = height/2;
 }
 
 function draw(){
@@ -44,6 +57,10 @@ function draw(){
   img2_y = img2_y + (mouseY-img2_y)/img2_followspeed;
   image(img2,img2_x,img2_y,img2.width * img1_scale,img2.height * img1_scale);
 
-  console.log(img2_x);
+  //shape 3
+  shape3y = (sin(shape3x/100) * height/4) + height/2; //function of a sin wave with a horizontal stretch of 100, a vertical compression of 4, and a vertical displacements downards of half of the canvas
+  shape3x += shape3xinc; //itterate through the xpositions
+  ellipse(shape3x,shape3y,shape3r); //draw the current x-y relationship of the sin function with a circle
+  console.log(shape3y);
 
 }
