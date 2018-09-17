40e42d7562aa9aa374bf0187e745720b54c5db39
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-09-17 14:14:28 -0400

    Got the second shape to follow the mouse at a delay
    I thought I would be able to acess the image's (x,y) directly via img2.x or img2.y but it seems that image-objects have no associated (x,y) positions so I created them myself with the variables img2_x and img2_y.
    I then found the difference in the distance between the current position of the image and the position of the mouse, and I then added a fraction of this difference to the image's position so it would slowly "catch up" to the mouse's position.

--- excercises/exercise1/js/script.js
+++ excercises/exercise1/js/script.js
@@ -8,12 +8,21 @@ const shape1xinc = 1; //constant at which to increment shape 1's xpos
 let img1;
 const img1_scale = 0.2;
 
+let img2;
+const img2_scale = 0.6;
+const img2_followspeed = 10; //the higher the number the less quickly img2 will catch up to the mouse;
+let img2_x = 0;
+let img2_y = 0;
 function preload(){
 img1 = loadImage("assets/images/thing1.png");
+
+img2 = loadImage("assets/images/thing2.png");
 }
 
 function setup(){
   createCanvas(displayWidth,displayHeight); //create canvas with the same dimensions as the screen resolution;
+
+  //shape 1
   shape1x = 0 - shape1r; //starting xpos of shape 1 set to just left of the drawn canvas
   shape1y = height/2; //starting ypos of shape 1 set to middle of screen;
 
@@ -21,10 +30,20 @@ function setup(){
 
 function draw(){
   background(255); //fill the screen with dark grey;
+
+  //shape1
   shape1x += shape1xinc;
   ellipse(shape1x,shape1y,shape1r); //draw shape 1
+
+  //image 1
   imageMode(CENTER);
   image(img1,mouseX,mouseY,img1.width * img1_scale,img1.height * img1_scale);
 
+  //image 2
+  img2_x = img2_x + (mouseX-img2_x)/img2_followspeed;
+  img2_y = img2_y + (mouseY-img2_y)/img2_followspeed;
+  image(img2,img2_x,img2_y,img2.width * img1_scale,img2.height * img1_scale);
+
+  console.log(img2_x);
 
 }
