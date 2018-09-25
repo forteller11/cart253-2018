6094d6ea1288656d91a08a3bfcb69d3d53e3c9e4
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-09-25 17:03:32 -0400

    Added sausage-dog to array

--- excercises/exercise3/js/script.js
+++ excercises/exercise3/js/script.js
@@ -9,16 +9,18 @@ author, and this description to match your project!
 ******************/
 let dogIndex = [];
 let dog = [];
-let dogPop = 200; //population of dogs
+let dogPop = 20; //population of dogs
+let dogImageNumber = 11; //number of dog images
+
 function preload() {
 let ii = i;
-  for (var i = 0; i <= 10; i++) { //fills the array with all dog images
+  for (var i = 0; i <= dogImageNumber; i++) { //fills the array with all strings of all dog repositories
     ii = i;
     dogIndex[i-1] = "assets/images/animals-"+ii+".png";
 
   }
   //loads a random dogimage
-  for (var i = 0; i <= 9; i++) {
+  for (var i = 0; i <= dogImageNumber-1; i++) {
     dog[i] = loadImage(dogIndex[i]);
   }
 }
@@ -27,8 +29,11 @@ function setup() {
   imageMode(CENTER);
   createCanvas(windowWidth,windowHeight);
   background(250,240,70);
+
+  //spawn dogs
   for (let i = 0; i < dogPop; i++ ) {
   image(dog[round(random(9))],random(width),random(height));
+  //spawn special dog;
 }
 
 
