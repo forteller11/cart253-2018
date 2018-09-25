bdc6c417ee2ba1c5516960da1912914b5083fda4
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-09-25 15:40:49 -0400

    Created an array to hold all dog images

--- excercises/exercise3/js/script.js
+++ excercises/exercise3/js/script.js
@@ -1,18 +1,25 @@
 /*****************
 
 Title of Project
-Author Name
+Charly Yan Miller
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let dogIndex = [];
+let dog;
+function preload() {
+let ii = i;
+  for (var i = 0; i <= 10; i++) { //fills the array with all dog images
+    ii = i;
+    dogIndex[i-1] = "assets/images/animals-"+ii+".png";
+  }
+  //loads a random dogimage
+  dog = loadImage( dogIndex[round(random(9))] );
 
-// setup()
-//
-// Description of setup
-
-
-// draw()
-//
-// Description of draw()
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  image(dog, 0, 0);
+}
