3a4a632c619c8bb3d1ba3862af7303b782708cb7
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-09-25 21:55:11 -0400

    ex3 clicking on waldo makes all other dogs dissappear
    All animals are drawn once in setup except for Waldo who is drawn in the draw function; therefore once it is desirable to only display waldo (when you won) the screen is filled with a solid color.

--- excercises/exercise3/js/script.js
+++ excercises/exercise3/js/script.js
@@ -5,20 +5,23 @@ Charly Yan Miller
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
-
+Sidenote: I kinda thought all the animals were dogs for some reason,
+so because of this var names tend to feature the word "dog";
 ******************/
 let dogIndex = [];
 let dog = [];
-let dogPop = 2; //population of dogs
+let dogPop = 20; //population of dogs
 let dogImageNumber = 11; //number of dog images in assets/images
 let waldoX;
 let waldoY;
 let framesPressed; //tracks the frames for which mouse has been pressed
 let waldoDisplayW = 400; //size of canvas in which to display target dog (waldo)
 let waldoDisplayH = waldoDisplayW;
+let waldoIndex; //make index of waldo a global var
 let playSpaceH = waldoDisplayH*8; //play space is area to spawn dogs in
 let avgImgW = 128; //avg img width
 let fontSize = 60;
+let win = false; //have you found waldo? FALSE
 function preload() {
 //fills the dogIndex array with strings/directories to all dog images
   for (var i = 0; i <= dogImageNumber; i++) {
@@ -34,17 +37,17 @@ function preload() {
 function setup() {
   //create canvas, fill WaldoDisplay and playarea with distinct colors
   createCanvas(400,waldoDisplayH+playSpaceH);
-  imageMode(CORNER);
+
   noStroke();
   fill(250,240,70);//yellow
   rect(0,0,waldoDisplayW,waldoDisplayH); //fills in UI space
-  imageMode(CORNER);
+
   fill(250,40,20);//red
   rect(0,waldoDisplayH,width,height); //fills in dog-space;
-  imageMode(CENTER);
+
 
   //set index of waldo (animal to be found)
-  let waldoIndex = round(random(9));
+  waldoIndex = round(random(9));
 
   //spawn random dogs randomly
   for (let i = 0; i < dogPop; i++ ) {
@@ -56,23 +59,16 @@ function setup() {
   }
 
 
-  //spawn and display waldo in play-space
-  imageMode(CENTER);
+  //set vars of waldo (position in play space)
   waldoX = random(waldoDisplayW);
   waldoY = random(playSpaceH)+waldoDisplayH;
-
-
-  image(dog[waldoIndex],waldoX,waldoY);
   waldoWidth = dog[waldoIndex].width/1.5;
-  print("waldo Width:"+waldoWidth);
-  //spawns waldo in user interface space
-  image(dog[waldoIndex],width/2,waldoDisplayH/2+dog[waldoIndex].width/3,dog[waldoIndex].width*2,dog[waldoIndex].height*2);
-  textSize(fontSize);
-  textAlign(CENTER);
-  text("find me",width/2,fontSize*1.4);
 }
 
 function draw(){
+  //fill displaySPace with yellow
+  fill(250,240,70);//yellow
+  rect(0,0,waldoDisplayW,waldoDisplayH); //fills in UI space
 
 if (mouseIsPressed){
   framesPressed ++;
@@ -85,12 +81,34 @@ else {
     if ( (mouseX > waldoX-waldoWidth/2) && (mouseX < waldoX + waldoWidth/2) ){
       if ((mouseY > waldoY - waldoWidth/2) && (mouseY < waldoY + waldoWidth/2)){
           print("I FOUND WALDO");
+          win = true;
+
       }
     }
-}
+  }
 
 
+  if (win === true){
+    fill(250,40,20,20);//red
+    rect(0,waldoDisplayH,width,height); //fills in dog-space;
+    //display text in user interface space
+    textSize(fontSize*0.7);
+    textAlign(CENTER);
+    fill(250,40,20);
+    text("YOU FOUND ME",width/2,fontSize*1.4);
+  }
+  else {
+    //display text in user interface space
+    fill(250,40,20);
+    textSize(fontSize);
+    textAlign(CENTER);
+    text("find me",width/2,fontSize*1.4);
+  }
 
-
+  imageMode(CENTER);
+  //draws waldo in user interface space
+  image(dog[waldoIndex],width/2,waldoDisplayH/2+dog[waldoIndex].width/3,dog[waldoIndex].width*2,dog[waldoIndex].height*2);
+  //draw waldo in game-space
+  image(dog[waldoIndex],waldoX,waldoY);
 
 }
