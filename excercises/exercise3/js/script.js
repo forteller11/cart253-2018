2ca07c0846a5e8cfc98d7efee1d30540121f6a06
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-09-29 17:47:48 -0400

    ex3 changing aesthetics on win
    very dissapointing that I cannot display or move all the instances of the face class every frame, so I'm resorting to randomly changing the properties of one face instance every frame post-win

--- excercises/exercise3/js/script.js
+++ excercises/exercise3/js/script.js
@@ -14,7 +14,7 @@ let face = []; //containg face objs
 let findMe; //img of text "find me"
 let youFoundMe; //img of text "YOU FOUND ME"
 let frame; //img of frame sourronding UI
-let facePop = 30; //population of faces
+let facePop = 90; //population of faces
 let faceImageNumber = 11; //number of face-images in assets/images
 let waldoX;
 let waldoXinitial;
@@ -72,13 +72,12 @@ function setup() {
   //spawn all non-waldo faces
   for (let i = 0; i < facePop; i++ ) {
   face[i] = new Face;
-  face[i].display();
+  face[i].display(255);
   }
 }
 
 function draw(){
-  //fill displaySPace with yellow
-//  background(r,g,b);
+
 
 //Allows program to know if mouse is being pressed, held, and for how many frames
 if (mouseIsPressed){
@@ -100,12 +99,16 @@ else {
 
   //if waldo has been clicked
   if (win === true){
-    fill(r,g,b);
+  //  fill(r,g,b);
     rect(0,0,waldoDisplayW,waldoDisplayH); //fills in UI space
     //slowly fill in playspace with color, obscuring all non-waldo animals
-    fill(r,g,b,20);
+    fill(r,g,b,4);
     rect(0,waldoDisplayH,width,height);
     //display  YOU FOUND ME in user interface space
+    for (i = 0; i < facePop; i ++){
+      //face[i].move(7,0);
+    }
+    face[round(random(facePop-1))].display(50);
     image(youFoundMe,width/2,waldoDisplayH/7,youFoundMe.width*waldoDisplayW/900,youFoundMe.height*waldoDisplayW/900);
     //interpolate playspace waldo's position until it equals UI waldo
     if (waldoYinc > -1){
@@ -139,11 +142,11 @@ else {
   image(faceImg[waldoIndex],width/2,waldoDisplayH/1.6,waldoDisplayW/1.4,waldoDisplayW/1.4);
   image(frame,width/2,waldoDisplayH/1.6,waldoDisplayW/1.3,waldoDisplayW/1.3);
 
-  //draw face populationfor (let i = 0; i < facePop; i++ ) {
 
 
 
 }
+
 class Face {
   constructor () {
   //spawn non waldo-animals randomly in play-space
@@ -157,7 +160,9 @@ class Face {
     this.h = imgW;
   }
 
-  display (){
+  display (a){
+    //a = opacity 1-255 of image;
+    tint(255,a);
     image(faceImg[this.index],this.x,this.y,this.w,this.h);
   }
 
@@ -167,6 +172,20 @@ class Face {
 
     this.x += x;
     this.y += y;
+
+    //screenwrap
+    if (this.x - this.w/2 > width) { //if off right
+      this.x = 0-this.w/2;
+    }
+    else if (this.x < 0 - this.w/2) { //if off left
+      this.x = width+this.w/2;
+    }
+    else if (this.y - this.w/2 > height) { //if off right
+      this.y = 0-this.w/2;
+    }
+    else if (this.y < 0 - this.w/2) { //if off left
+      this.y = height+this.w/2;
+    }
   }
 
 
