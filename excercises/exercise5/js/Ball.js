2b4f38dce879d3d3d827d5025d391d008e4b1a93
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-10-20 16:16:00 -0400

    ex5 removed es6 ways of creating a ball class
    Now every New ball has its functions (instead of calling functions from a single proto_ball which I am working towards)

--- excercises/exercise5/js/Ball.js
+++ excercises/exercise5/js/Ball.js
@@ -1,23 +1,22 @@
-class Ball {
-  constructor() {
-    this.x;
-    this.y;
-    this.xHist = []; //history of x positions
-    this.yHist = []; //history of y positions
-    this.trailLength = 12;
-    this.maxVelX = 19;
-    this.velX = 0;
-    this.velY = 0;
-    this.radius = 8;
-    //rgb values
-    this.r = 255;
-    this.g = 255;
-    this.b = 255;
-    this.timer = 0;
-    this.hit = 0;
-  }
+function Ball() {
+  this.x;
+  this.y;
+  this.xHist = []; //history of x positions
+  this.yHist = []; //history of y positions
+  this.trailLength = 12;
+  this.maxVelX = 19;
+  this.velX = 0;
+  this.velY = 0;
+  this.radius = 8;
+  //rgb values
+  this.r = 255;
+  this.g = 255;
+  this.b = 255;
+  this.timer = 0;
+  this.hit = 0;
 
-  changePosition() {
+
+  this.changePosition = function() {
     this.x += this.velX;
     this.y += this.velY;
     this.canvasCollision(); //has the ball collided with the canvas?
@@ -27,7 +26,7 @@ class Ball {
 
   }
 
-  addToHistory(x, y) { //remove oldest ball postion history, add newest ball position history
+  this.addToHistory = function(x, y) { //remove oldest ball postion history, add newest ball position history
     this.xHist.splice(0, 1);
     this.yHist.splice(0, 1);
 
@@ -35,7 +34,7 @@ class Ball {
     this.yHist.push(this.y);
   }
 
-  reset(xDirection) {
+  this.reset = function(xDirection) {
     //resets pos, color and velocity of ball
     this.r = 255;
     this.g = 255;
@@ -56,7 +55,7 @@ class Ball {
     }
   }
 
-  display() {
+  this.display = function() {
     rectMode(RADIUS);
     noStroke();
 
@@ -69,21 +68,21 @@ class Ball {
     //ellipse(this.x,this.y,this.radius,this.radius);
   }
 
-  displayTrail() { //draw trail of ball's histories
+  this.displayTrail = function() { //draw trail of ball's histories
     rectMode(RADIUS);
     noStroke();
     for (let i = 0; i < this.xHist.length; i++) {
       //trail increases alpha based on balls speed,
       //alpha is greatest near the current position of the ball, then it fades to 0
       fill(this.r, this.g, this.b, i * (abs((ball.velX + ball.velY) * 15) / this.trailLength));
-      let rad = (this.radius*i)/this.xHist.length; //fades radius to 0 at the end of the for Loop
-      let rad2 = map(rad,0,this.radius,this.radius*0.6,this.radius); //setting min radius of trail to 60%
+      let rad = (this.radius * i) / this.xHist.length; //fades radius to 0 at the end of the for Loop
+      let rad2 = map(rad, 0, this.radius, this.radius * 0.6, this.radius); //setting min radius of trail to 60%
       rect(this.xHist[i], this.yHist[i], rad2, rad2);
     }
     //ellipse(this.x,this.y,this.radius,this.radius);
   }
 
-  canvasCollision() {
+  this.canvasCollision = function() {
     //if ball goes off left then rightpaddle score increases
     if (this.x + (this.radius * 4) < 0) {
       padR.score++;
@@ -104,14 +103,14 @@ class Ball {
     }
     //if ball hits ceiling or floor of canvas
     if ((this.y + this.radius > height) || (this.y - this.radius < 0)) {
-      this.y = constrain(this.y, this.radius, height-this.radius);
+      this.y = constrain(this.y, this.radius, height - this.radius);
       this.velY = this.velY * -1;
       let changeInOscFreq = abs(ball.velY) * 10; //more change if the ball is traveling faster vertically
       oscAmbienceFreq += changeInOscFreq; //increases frequency of oscillator
     }
   }
 
-  decrHit() { //decrements this.hit var
+  this.decrHit = function() { //decrements this.hit var
     //this.hit is set to one on collison w/ball
     if (this.hit > 0) {
       this.hit -= 0.15;
