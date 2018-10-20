c5a08ba60834d45ca88a9230be1c832a55e0e4cb
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-10-20 16:46:05 -0400

    ex5 changing visuals --> making them more dynamic
    Now the time that the ball is white after collision increases with the xspeed of ball post collision, now paddle's fillMeters change based on the xSpeed of ball post collision (rather then where they are on the horizontal play-space) and now the meter shrinks instead of fades in opacity

--- excercises/exercise5/js/Ball.js
+++ excercises/exercise5/js/Ball.js
@@ -16,103 +16,103 @@ function Ball() {
   this.hit = 0;
 }
 
-  Ball.prototype.changePosition = function() {
-    this.x += this.velX;
-    this.y += this.velY;
-    this.canvasCollision(); //has the ball collided with the canvas?
-    this.addToHistory(this.x, this.y); //store balls postional history for trail drawing
+Ball.prototype.changePosition = function() {
+  this.x += this.velX;
+  this.y += this.velY;
+  this.canvasCollision(); //has the ball collided with the canvas?
+  this.addToHistory(this.x, this.y); //store balls postional history for trail drawing
 
-    this.velX = constrain(this.velX, -this.maxVelX, this.maxVelX);
+  this.velX = constrain(this.velX, -this.maxVelX, this.maxVelX);
+
+}
+
+Ball.prototype.addToHistory = function(x, y) { //remove oldest ball postion history, add newest ball position history
+  this.xHist.splice(0, 1);
+  this.yHist.splice(0, 1);
+
+  this.xHist.push(this.x);
+  this.yHist.push(this.y);
+}
+
+Ball.prototype.reset = function(xDirection) {
+  //resets pos, color and velocity of ball
+  this.r = 255;
+  this.g = 255;
+  this.b = 255;
+  this.x = width / 2;
+  this.y = height / 2 + (random(-width / 3, width / 3));
+  //x direction is either 1 or -1 depending on which paddle won
+  this.velX = random(2, 3) * xDirection;
+  this.velY = random(-2, 2);
+
+  //fill array at start of game to trailLength
+  for (let i = 0; i < this.trailLength; i++) {
+    this.xHist[i] = this.x;
+    this.yHist[i] = this.y;
+
+    this.xHist[i] = this.x;
+    this.yHist[i] = this.y;
+  }
+}
+
+Ball.prototype.display = function() {
+  rectMode(RADIUS);
+  noStroke();
+
+  //draw coloured ball
+  fill(this.r, this.g, this.b);
+  rect(this.x, this.y, this.radius, this.radius);
+  //draw a white ball which obscures the undlying colored one depending on this.hit
+  fill(255, 255, 255, this.hit * 255);
+  rect(this.x, this.y, this.radius, this.radius);
+  //ellipse(this.x,this.y,this.radius,this.radius);
+}
+
+Ball.prototype.displayTrail = function() { //draw trail of ball's histories
+  rectMode(RADIUS);
+  noStroke();
+  for (let i = 0; i < this.xHist.length; i++) {
+    //trail increases alpha based on balls speed,
+    //alpha is greatest near the current position of the ball, then it fades to 0
+    fill(this.r, this.g, this.b, i * (abs((ball.velX + ball.velY) * 15) / this.trailLength));
+    let rad = (this.radius * i) / this.xHist.length; //fades radius to 0 at the end of the for Loop
+    let rad2 = map(rad, 0, this.radius, this.radius * 0.6, this.radius); //setting min radius of trail to 60%
+    rect(this.xHist[i], this.yHist[i], rad2, rad2);
+  }
+  //ellipse(this.x,this.y,this.radius,this.radius);
+}
+
+Ball.prototype.canvasCollision = function() {
+  //if ball goes off left then rightpaddle score increases
+  if (this.x + (this.radius * 4) < 0) {
+    padR.score++;
+    padR.scored = .5;
+    padR.hit = 1; //fill the scoring paddle with colour
+    this.reset(1);
+    oscAmbienceFreq += 70; //increases frequency of oscillator
 
   }
+  //if ball goes off right then leftpaddle score increases
+  if (this.x - (this.radius * 4) > width) {
+    padL.score++;
+    padL.scored = .5;
+    padL.hit = 1; //fill the scoring paddle with colour
+    this.reset(-1);
+    oscAmbienceFreq += 70;
 
-  Ball.prototype.addToHistory = function(x, y) { //remove oldest ball postion history, add newest ball position history
-    this.xHist.splice(0, 1);
-    this.yHist.splice(0, 1);
-
-    this.xHist.push(this.x);
-    this.yHist.push(this.y);
   }
-
-  Ball.prototype.reset = function(xDirection) {
-    //resets pos, color and velocity of ball
-    this.r = 255;
-    this.g = 255;
-    this.b = 255;
-    this.x = width / 2;
-    this.y = height / 2 + (random(-width / 3, width / 3));
-    //x direction is either 1 or -1 depending on which paddle won
-    this.velX = random(2, 3) * xDirection;
-    this.velY = random(-2, 2);
-
-    //fill array at start of game to trailLength
-    for (let i = 0; i < this.trailLength; i++) {
-      this.xHist[i] = this.x;
-      this.yHist[i] = this.y;
-
-      this.xHist[i] = this.x;
-      this.yHist[i] = this.y;
-    }
+  //if ball hits ceiling or floor of canvas
+  if ((this.y + this.radius > height) || (this.y - this.radius < 0)) {
+    this.y = constrain(this.y, this.radius, height - this.radius);
+    this.velY = this.velY * -1;
+    let changeInOscFreq = abs(ball.velY) * 10; //more change if the ball is traveling faster vertically
+    oscAmbienceFreq += changeInOscFreq; //increases frequency of oscillator
   }
+}
 
-  Ball.prototype.display = function() {
-    rectMode(RADIUS);
-    noStroke();
-
-    //draw coloured ball
-    fill(this.r, this.g, this.b);
-    rect(this.x, this.y, this.radius, this.radius);
-    //draw a white ball which obscures the undlying colored one depending on this.hit
-    fill(255, 255, 255, this.hit * 255);
-    rect(this.x, this.y, this.radius, this.radius);
-    //ellipse(this.x,this.y,this.radius,this.radius);
+Ball.prototype.decrHit = function() { //decrements this.hit var
+  //this.hit is set to one on collison w/ball
+  if (this.hit > 0) {
+    this.hit -= 0.15;
   }
-
-  Ball.prototype.displayTrail = function() { //draw trail of ball's histories
-    rectMode(RADIUS);
-    noStroke();
-    for (let i = 0; i < this.xHist.length; i++) {
-      //trail increases alpha based on balls speed,
-      //alpha is greatest near the current position of the ball, then it fades to 0
-      fill(this.r, this.g, this.b, i * (abs((ball.velX + ball.velY) * 15) / this.trailLength));
-      let rad = (this.radius * i) / this.xHist.length; //fades radius to 0 at the end of the for Loop
-      let rad2 = map(rad, 0, this.radius, this.radius * 0.6, this.radius); //setting min radius of trail to 60%
-      rect(this.xHist[i], this.yHist[i], rad2, rad2);
-    }
-    //ellipse(this.x,this.y,this.radius,this.radius);
-  }
-
-  Ball.prototype.canvasCollision = function() {
-    //if ball goes off left then rightpaddle score increases
-    if (this.x + (this.radius * 4) < 0) {
-      padR.score++;
-      padR.scored = .5;
-      padR.hit = 1; //fill the scoring paddle with colour
-      this.reset(1);
-      oscAmbienceFreq += 70; //increases frequency of oscillator
-
-    }
-    //if ball goes off right then leftpaddle score increases
-    if (this.x - (this.radius * 4) > width) {
-      padL.score++;
-      padL.scored = .5;
-      padL.hit = 1; //fill the scoring paddle with colour
-      this.reset(-1);
-      oscAmbienceFreq += 70;
-
-    }
-    //if ball hits ceiling or floor of canvas
-    if ((this.y + this.radius > height) || (this.y - this.radius < 0)) {
-      this.y = constrain(this.y, this.radius, height - this.radius);
-      this.velY = this.velY * -1;
-      let changeInOscFreq = abs(ball.velY) * 10; //more change if the ball is traveling faster vertically
-      oscAmbienceFreq += changeInOscFreq; //increases frequency of oscillator
-    }
-  }
-
-  Ball.prototype.decrHit = function() { //decrements this.hit var
-    //this.hit is set to one on collison w/ball
-    if (this.hit > 0) {
-      this.hit -= 0.15;
-    }
-  }
+}
