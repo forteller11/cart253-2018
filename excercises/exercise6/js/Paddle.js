e27f7cb8880da5823d90e4ff8d6524a93c48055a
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-10-23 18:05:07 -0400

    ex6 spelt hieght correctly

--- excercises/exercise6/js/Paddle.js
+++ excercises/exercise6/js/Paddle.js
@@ -36,7 +36,7 @@ Paddle.prototype.handleInput = function() { //NEW spelt protoytpe correctly
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  this.y = constraint(this.y,0,hight-this.h);
+  this.y = constraint(this.y,0,height-this.h);
 }
 
 // display()
