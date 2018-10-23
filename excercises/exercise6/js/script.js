982c9d5370a53540a5ba701352ca407780e407cc
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-10-23 16:53:23 -0400

    ex6
    set a more reasonable this.w value in the rightPaddle constructor argument

--- excercises/exercise6/js/script.js
+++ excercises/exercise6/js/script.js
@@ -23,7 +23,7 @@ function setup() {
   // Create a ball
   ball = new Ball(width/2,height/2,50,50,10,50);
   // Create the right paddle with UP and DOWN as controls
-  rightPaddle = new Paddle(width-10,height/2,10,600,10,UP_ARROW,DOWN_ARROW);
+  rightPaddle = new Paddle(width-10,height/2,10,60,10,UP_ARROW,DOWN_ARROW); //NEW set a more reasonable this.w value
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
   //////////New brackets were left open on constructor function, closed brackets
