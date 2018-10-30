fd1e18d68f3b70862725a2a68773ad7a697d07b1
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-10-30 14:18:41 -0400

    project 2 started project2!

---
+++ projects/project2/js/script.js
@@ -0,0 +1,23 @@
+/*
+Charly Yan Miller
+Project 2
+2D pong with lights
+
+
+Credits:
+Used developer Nick Liow's toturial on 2D ray casting/lighting "Sight and Light"
+(no pseudo-code was copied)
+https://ncase.me/sight-and-light/
+
+Used a series of Khan academy's toturials by Salman Khan's on parametric lines
+https://www.khanacademy.org/math/linear-algebra/vectors-and-spaces/vectors/v/linear-algebra-parametric-representations-of-lines
+https://www.khanacademy.org/math/ap-calculus-bc/bc-advanced-functions-new/bc-9-1/v/parametric-equations-1
+https://www.youtube.com/watch?v=qksmRZNaqJY
+*/
+function setup(){
+  createCanvas(800,800);
+}
+
+function draw(){
+  background(50,50);
+}
