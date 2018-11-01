0c9a0fbd896b0cd41cea3007f3d36ff8125269ea
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-11-01 13:58:42 -0400

    proj 2 visualizing sorting algorithim properly

--- projects/project2/js/Ray.js
+++ projects/project2/js/Ray.js
@@ -1,5 +1,5 @@
-class Ray{
-  constructor(targetX,targetY){
+class Ray {
+  constructor(targetX, targetY) {
     this.x;
     this.y;
     // this.angle = angle;
@@ -17,59 +17,57 @@ class Ray{
     y unit vectors
     */
   }
-  update(){
+  update() {
     //set origin
     this.x = mouseX;
     this.y = mouseY;
     // this.targetX = 0;
     this.checkIntersection();
     this.calculateAngle();
-
-
   }
 
-  checkIntersection(){
+  checkIntersection() {
     //check if not infinit, parrellel, 0
 
     //convert point form to y = mx+b
     //find slope
     this.collidedX = Infinity;
     this.collidedY = Infinity;
-    let raySlope = (this.targetY - this.y)/(this.targetX - this.x);
+    let raySlope = (this.targetY - this.y) / (this.targetX - this.x);
 
     //find y intercept
-    let rayB = this.y-(raySlope*this.x);
+    let rayB = this.y - (raySlope * this.x);
 
-    for (let i = 0; i < shape.length; i ++){
-      for (let j = 0; j < shape[i].lines.length; j++){
+    for (let i = 0; i < shape.length; i++) {
+      for (let j = 0; j < shape[i].lines.length; j++) {
 
         // let line = shape[i].lines[j];
         let line = shape[i].lines[j];
-        let lineSlope = (line.y2-line.y1)/(line.x2 - line.x1);
-        let lineB = line.y1-(lineSlope*line.x1);
+        let lineSlope = (line.y2 - line.y1) / (line.x2 - line.x1);
+        let lineB = line.y1 - (lineSlope * line.x1);
 
         //check to where ray and line intersect
         let simplifyB = lineB - rayB;
         let simplifySlope = raySlope - lineSlope;
-        let intersectionX = simplifyB/simplifySlope; //the x location where lines intersects
-        let intersectionY = (raySlope*intersectionX)+rayB; //y where lines interesct
+        let intersectionX = simplifyB / simplifySlope; //the x location where lines intersects
+        let intersectionY = (raySlope * intersectionX) + rayB; //y where lines interesct
 
-        stroke(255,255,255,50);
+        stroke(255, 255, 255, 50);
         //make sure only recognizes intersections which happen in the direction of the ray
-        if (((this.targetX-this.x<= 0) && (intersectionX < this.x)) || ((this.targetX-this.x > 0) && (intersectionX > this.x))){
+        if (((this.targetX - this.x <= 0) && (intersectionX < this.x)) || ((this.targetX - this.x > 0) && (intersectionX > this.x))) {
           //check to see if collsion happened within confines of line (and not infinite funciton)
           let c = 0.0000000001; //creates a buffer so the intersection doesn't have to happen exactly on the line to be recognized
           if (line.x1 < line.x2) {
-            if ( (intersectionX+c >= line.x1) && (intersectionX-c <= line.x2) ){
+            if ((intersectionX + c >= line.x1) && (intersectionX - c <= line.x2)) {
               // ellipse(intersectionX,intersectionY,10);
               //change collidedX,Y to the intersection's X,Y's are closer to the ray origin
-              this.makeCollidedShortestIntersection(intersectionX,intersectionY);
+              this.makeCollidedShortestIntersection(intersectionX, intersectionY);
             }
           } else if (line.x1 > line.x2) {
-            if ( (intersectionX-c <= line.x1) && (intersectionX+c >= line.x2) ){
-              ellipse(intersectionX,intersectionY,10);
+            if ((intersectionX - c <= line.x1) && (intersectionX + c >= line.x2)) {
+              ellipse(intersectionX, intersectionY, 10);
               //change collidedX,Y to the intersection's X,Y's are closer to the ray origin
-              this.makeCollidedShortestIntersection(intersectionX,intersectionY);
+              this.makeCollidedShortestIntersection(intersectionX, intersectionY);
             }
           }
         }
@@ -88,29 +86,29 @@ class Ray{
     }
   }
 
-  makeCollidedShortestIntersection(intersectionX,intersectionY){
+  makeCollidedShortestIntersection(intersectionX, intersectionY) {
     //compares the dist between the ray origin and the two vectors,
     //and if intersection is shorter then it makes collidedX,y the intersection's x,y
-    let collidedR = sqrt(sq(this.collidedX-this.x)+sq(this.collidedY-this.y));
-    let intersectionR = sqrt(sq(intersectionX-this.x)+sq(intersectionY-this.y));
-    if (intersectionR < collidedR){
+    let collidedR = sqrt(sq(this.collidedX - this.x) + sq(this.collidedY - this.y));
+    let intersectionR = sqrt(sq(intersectionX - this.x) + sq(intersectionY - this.y));
+    if (intersectionR < collidedR) {
       this.collidedX = intersectionX;
       this.collidedY = intersectionY;
     }
   }
 
-  calculateAngle(){
-    let radius = sqrt( sq(this.targetX-this.x) + sq(this.targetY-this.y) );
-    this.angle = asin( (this.targetY-this.y)/radius);
-     print(this.angle);
+  calculateAngle() {
+    let radius = sqrt(sq(this.targetX - this.x) + sq(this.targetY - this.y));
+    this.angle = asin((this.targetY - this.y) / radius);
+    print(this.angle);
   }
 
-  display(){
+  display() {
     strokeWeight(2);
-    stroke(255,100,0);
-    line(this.x,this.y,(this.collidedX),(this.collidedY));
-    ellipse(this.x,this.y,20);
-    ellipse(this.targetX,this.targetY,10);
+    stroke(255, 100, 0);
+    line(this.x, this.y, (this.collidedX), (this.collidedY));
+    ellipse(this.x, this.y, 20);
+    ellipse(this.targetX, this.targetY, 10);
   }
 
 }
--- projects/project2/js/sorting.js
+++ projects/project2/js/sorting.js
@@ -1,6 +1,8 @@
 let ray = [];
 let i = 0;
 let j = 0;
+let smallestValue = Infinity;
+let smallestValueIndex;
 
 function setup() {
   createCanvas(100, 100);
@@ -13,13 +15,18 @@ function setup() {
 function draw() {
   background(0);
 
-  if (j < ray.length){
+  if (j < ray.length) {
+    // print(ray[j] + "vs" + smallestValue);
+    if(ray[j] < smallestValue){
+      // print("ray is smaller");
+      smallestValueIndex = j;
+      smallestValue = ray[j];
+    }
+
     j++;
-
-
-  } else if (i < ray.length){
-      i++;
-      j=0;
+  } else if (i < ray.length) {
+    i++;
+    j = i;
 
   }
 
@@ -27,13 +34,15 @@ function draw() {
 
   for (let i = 0; i < width; i++) {
     stroke(200);
-    line(i, height, i, ray[i]);
+    line(i, height,i,height-ray[i]);
   }
+    print(ray[0]);
 
+  stroke(255, 0, 0);
+  line(j, height,j,height-ray[j]);;
+  stroke(100, 100, 255);
+    line(i, height,i,0);;
+  stroke(100, 255, 100);
+  line(smallestValueIndex, height, smallestValueIndex, height-smallestValue);
 
-  stroke(255,0,0);
-  line(j, height, j, ray[j]);
-  stroke(100,100,255);
-  line(i, height, i, 0);
-
-  }
+}
