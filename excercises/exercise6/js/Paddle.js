931bab6b12c28fb3620a2dc72bb011f562e15688
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-11-03 16:02:16 -0400

    ex6 repo fix
    my last ex6 commit i deleted almost everything by accident and didn't realize, so I just copy pasted stuff from earlier commits to make the repo functioning again.

---
+++ excercises/exercise6/js/Paddle.js
@@ -0,0 +1,50 @@
+// Paddle
+//
+// A class that defines how a paddle behaves, including the ability
+// to specify the input keys to move it up and down
+
+//Paddle constructor ///////NEW commented out this comment
+
+//Sets the properties with the provided arguments or defaults //////NEW commented out this comment
+function Paddle(x,y,w,h,speed,downKey,upKey) { ///////NEW spelt "Paddle" correctly
+  this.x = x;
+  this.y = y;
+  this.vx  = 0; //NEW correctly spelt vx
+  this.vy = 0; //NEW correctly spelt vy
+  this.w = w;
+  this.h = h;
+  this.speed = speed; //NEW spelt speed correctly
+  this.downKey = downKey;
+  this.upKey = upKey;
+}
+
+// handleInput()
+//
+// Check if the up or down keys are pressed and update velocity
+// appropriately
+Paddle.prototype.handleInput = function() { //NEW spelt protoytpe correctly
+  if (keyIsDown(this.upKey)) { //NEW correctly spelt function keyIs Down,added this to upKey
+    this.vy = -this.speed;
+  }
+  else if (keyIsDown(this.downKey)) { //NEW correctly spelt function keyIsDown, added this to downKey
+    this.vy = this.speed; //NEW corrected added to speed (rather than substract)
+  }
+  else { //NEW made it so paddles now stop moving if keys aren't being pressed
+    this.vy = 0;
+  }
+}
+
+// update()
+// Update y position based on velocity
+// Constrain the resulting position to be within the canvas
+Paddle.prototype.update = function() {
+  this.y += this.vy;
+  this.y = constrain(this.y,0,height-this.h); //NEW spelt height correctly, spelt constrain correctly
+}
+
+// display()
+//
+// Draw the paddle as a rectangle on the screen
+Paddle.prototype.display = function() { //NEW removed extra brackets, NEW spelled display correctly
+  rect(this.x,this.y,this.w,this.h); //spelled rect correctly
+}
