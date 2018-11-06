bcd3502bc97acbc1f542940c687d7ee28002c023
Charly Yan Miller <42951087+forteller11@users.noreply.github.com>
2018-11-06 16:49:47 -0500

    proj 2, commented ray class

--- projects/project2/js/Ray.js
+++ projects/project2/js/Ray.js
@@ -27,36 +27,32 @@ class Ray {
     this.collidedX; //closest point of x intersection
     this.collidedY; //closest point of y intersection
     this.angle; //angle
-    this.children = [];
-    this.hasChildren; //true or false, does this ray have children?
+    this.hasChildren = createChildren; //true or false, does this ray have children?
+    this.children = []; //the array where any children will be put
     this.debug = false;
+    //if set to create children create two rays who are set NOT to create children.
     if (createChildren === true) {
-      this.hasChildren = true;
       this.children[0] = new Ray(targetX, targetY, false);
       this.children[1] = new Ray(targetX, targetY, false);
-
-    } else {
-      this.hasChildren = false;
     }
 
   }
   update() {
     if (this.hasChildren === true) { //if you have children (and are therefore not a child)
-      this.checkIntersection();
-      this.calculateAngle();
-      this.updateChildren();
+      this.checkIntersection(); //sees where the ray intersects with lines, and stores the closest collision
+      this.calculateAngle(); //caluclates the angle of the ray (cartesian-->polar)
+      this.updateChildren(); //sets offsets children's angle/targetx,y; updates chilldren
     }
   }
   updateChildren() {
     for (let i = 0; i < this.children.length; i++) {
-      //offsets angle slightly each direction
-      let angleOffset = .00001
+      //offsets angle of children slightly in each direction
+      let angleOffset = 0.00001
       if (i === 0) {
         angleOffset = -angleOffset;
       }
-
       this.children[i].angle = this.angle + angleOffset;
-      // print(i+":"+this.angle);
+      //sets the new target of children based on their new offset angle
       this.calculateTargetBasedOnAngle(this.angle + angleOffset, i);
       this.children[i].x = this.x;
       this.children[i].y = this.y;
@@ -67,51 +63,60 @@ class Ray {
   }
 
   checkIntersection() {
-    //find slope
+    //vars store the closest point(x,y) of collision
     this.collidedX = Infinity;
     this.collidedY = Infinity;
+
+    //transform ray into linear function in slope form (y = mx+b)
+    //find slope
     let raySlope = (this.targetY - this.y) / (this.targetX - this.x);
-
-    //find y intercept
+    //find y intercept (or "b")
     let rayB = this.y - (raySlope * this.x);
-
+    //itterate through all lines and check point of collision with ray, store the closest collision
     for (let i = 0; i < shape.length; i++) {
       for (let j = 0; j < shape[i].lines.length; j++) {
 
-        // let line = shape[i].lines[j];
+        //convert line to slope form (y = mx+b)
         let line = shape[i].lines[j];
         let lineSlope = (line.y2 - line.y1) / (line.x2 - line.x1);
         let lineB = line.y1 - (lineSlope * line.x1);
 
-        //check to where ray and line intersect
+        //check to see where ray and line intersect (assuming not parrallel and slope not 0, Infinity,-Infinity)
+        //uses basic algebra (isolation, substitution...)
         let simplifyB = lineB - rayB;
         let simplifySlope = raySlope - lineSlope;
         let intersectionX = simplifyB / simplifySlope; //the x location where lines intersects
         let intersectionY = (raySlope * intersectionX) + rayB; //y where lines interesct
 
 
-        //make sure only recognizes intersections which happen in the direction of the ray
+        //make sure collision happened in front of the ray
+        //(because ray is converted to slope form it becomes directionless meaning it will intersect with lines behind it)
         if (((this.targetX - this.x <= 0) && (intersectionX < this.x)) || ((this.targetX - this.x > 0) && (intersectionX > this.x))) {
-          //check to see if collsion happened within confines of line (and not infinite funciton)
-          let c = 0.0000000001; //creates a buffer so the intersection doesn't have to happen exactly on the line to be recognized
+          //now make sure the collision happanned in between the two end points of the line
+          //(instead of inevitably somewhere on the infinite linear function)
+          //"c" creates a buffer so the intersection doesn't have to happen exactly on the line to be recognized
+          //because of rounding errors of floating point numbers this small buffer is necessary
+          let c = 0.0000000001;
           if (line.x1 < line.x2) {
             if ((intersectionX + c >= line.x1) && (intersectionX - c <= line.x2)) {
-              if (debugDisplay === true){
+              if (debugDisplay === true){ //shows where intersections happen if debugMode is on
                 stroke(this.r, this.g, this.b,50);
                 fill(this.r, this.g, this.b);
                 ellipse(intersectionX,intersectionY,7);
               }
-              //change collidedX,Y to the intersection's X,Y's are closer to the ray origin
+              //if intersection happend closer than current stored closest collision
+              //change collisionX,Y to this be the points of this intersection
               this.makeCollidedShortestIntersection(intersectionX, intersectionY);
             }
           } else if (line.x1 > line.x2) {
             if ((intersectionX - c <= line.x1) && (intersectionX + c >= line.x2)) {
-              if (debugDisplay === true){
+              if (debugDisplay === true){ //shows where intersections happen if debugMode is on
                 stroke(this.r, this.g, this.b,50);
                 fill(this.r, this.g, this.b);
                 ellipse(intersectionX,intersectionY,7);
               }
-              //change collidedX,Y to the intersection's X,Y's are closer to the ray origin
+              //if intersection happend closer than current stored closest collision
+              //change collisionX,Y to this be the points of this intersection
               this.makeCollidedShortestIntersection(intersectionX, intersectionY);
             }
           }
@@ -121,8 +126,8 @@ class Ray {
     }
   }
   makeCollidedShortestIntersection(intersectionX, intersectionY) {
-    //compares the dist between the ray origin and the two vectors,
-    //and if intersection is shorter then it makes collidedX,y the intersection's x,y
+    //compares the dist between the ray origin and the intersectionX,Y and collidedX,Y
+    //and if intersection is shorter then it changes collision,x,y to be the intersection
     let collidedR = sqrt(sq(this.collidedX - this.x) + sq(this.collidedY - this.y));
     let intersectionR = sqrt(sq(intersectionX - this.x) + sq(intersectionY - this.y));
     if (intersectionR < collidedR) {
@@ -138,19 +143,21 @@ class Ray {
     let newAngle = atan2(yVec, xVec);
     newAngle = map(newAngle, -PI, PI, 0, PI * 2);
     this.angle = newAngle;
-    noStroke();
-    fill(180);
-    if (this.debug === true) {
+    if (this.debug === true) { //draws angles if debugMode is on
+      noStroke();
+      fill(180);
       text(round(this.angle * 10000) / 10000, this.collidedX + 20, this.collidedY);
     }
   }
-  calculateTargetBasedOnAngle(angle, index) {
+  calculateTargetBasedOnAngle(angle, index) { //polar-->Cartesian
     let radius = sqrt(sq(this.targetX - this.x) + sq(this.targetY - this.y));
     this.children[index].targetX = (-cos(angle) * radius) + this.x;
     this.children[index].targetY = (-sin(angle) * radius) + this.y;
   }
 
   display() {
+    //if debug mode is on display draws the lines on the rays, colors them
+    //according to their angle, and highlights point where the ray collided
     if (debugDisplay === true){
       strokeWeight(2);
       //calcs colour based on angle
