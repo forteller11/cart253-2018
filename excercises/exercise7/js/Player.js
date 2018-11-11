b28fc01c68954694e2fe7152205f2ee85a1e5d2c
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-11-11 01:12:13 -0500

    ex7 new strategy to turn rotate camera which is much smoother

--- excercises/exercise7/js/Player.js
+++ excercises/exercise7/js/Player.js
@@ -48,7 +48,6 @@ class Player {
     fill(255, 255, 255, 100);
     strokeWeight(4);
     // noStroke(0)
-    let wHist = 0;
     let angleDiffNet = 0;
     noStroke();
     rectMode(CORNERS);
@@ -56,11 +55,14 @@ class Player {
     // print(this.angle);
     //this.k is basically what controls rotations now, and it determines the starting point of i;
     //it should be determined based on width, and then should visually wrap over i think
-    this.k = round(map(this.angle,-PI,PI,0,this.parentRay.length - 1));
+    let wHist = round(map(this.angle, -PI, PI, width, 0));
+    print(wHist);
     // if (this.k =)
-    print(this.k);
-    for (let i = this.k; i < this.parentRay.length - 1; i++) {
 
+    for (let i = 0; i < this.parentRay.length - 1; i++) {
+      if (wHist > width) {
+        wHist = 0;
+      }
       let v0 = this.parentRay[i].children[0];
       let v1 = this.parentRay[i];
       let v2 = this.parentRay[i].children[1];
@@ -70,193 +72,148 @@ class Player {
       let aDiff1 = v2.angle - v1.angle;
       let aDiff2 = v3.angle - v2.angle;
 
-      let hBase = height/2;
+      let hBase = height / 2;
       let hTune = 1;
-      let dist0 = map(v0.collidedRad,0,width,height/2,0);
-      dist0 = constrain(dist0,0,height);
-      let dist1 = map(v1.collidedRad,0,width,height/2,0);
-      dist1 = constrain(dist1,0,height);
-      let dist2 = map(v2.collidedRad,0,width,height/2,0);
-      dist2 = constrain(dist2,0,height);
-      let dist3 = map(v3.collidedRad,0,width,height/2,0);
-      dist3 = constrain(dist3,0,height);
+      let dist0 = map(v0.collidedRad, 0, width, height / 2, 0);
+      dist0 = constrain(dist0, 0, height);
+      let dist1 = map(v1.collidedRad, 0, width, height / 2, 0);
+      dist1 = constrain(dist1, 0, height);
+      let dist2 = map(v2.collidedRad, 0, width, height / 2, 0);
+      dist2 = constrain(dist2, 0, height);
+      let dist3 = map(v3.collidedRad, 0, width, height / 2, 0);
+      dist3 = constrain(dist3, 0, height);
 
-      let hOff0 = (dist0*v1.collidedH)*hTune;
-      let hOff1 = (dist1*v1.collidedH)*hTune;
-      let hOff2 = (dist2*v1.collidedH)*hTune;
-      let hOff3 = (dist3*v1.collidedH)*hTune;
+      let hOff0 = (dist0 * v1.collidedH) * hTune;
+      let hOff1 = (dist1 * v1.collidedH) * hTune;
+      let hOff2 = (dist2 * v1.collidedH) * hTune;
+      let hOff3 = (dist3 * v1.collidedH) * hTune;
 
-      let opacityFill = map((v1.collidedRad),0,width,255,0);
+      let opacityFill = map((v1.collidedRad), 0, width, 255, 0);
       let w0 = map(aDiff0, 0, TWO_PI, 0, width);
       let w1 = map(aDiff1, 0, TWO_PI, 0, width);
       let w2 = map(aDiff2, 0, TWO_PI, 0, width);
       // rect(wHist, hBase+hOff1, wHist + w, hBase-hOff1);
-      fill(v1.collidedR,v1.collidedG,v1.collidedB,opacityFill)
+      fill(v1.collidedR, v1.collidedG, v1.collidedB, opacityFill)
       beginShape();
-      vertex(wHist,hBase-hOff0); //topleft
-      vertex(wHist+w0,hBase-hOff1);
-      vertex(wHist+w0+w1,hBase-hOff2);
-      vertex(wHist+w0+w1+w2,hBase-hOff3); //topright
-      vertex(wHist+w0+w1+w2,hBase+hOff3); //botright
-      vertex(wHist+w0+w1,hBase+hOff2);
-      vertex(wHist+w0,hBase+hOff1);
-      vertex(wHist,hBase+hOff0); //botleft
+      vertex(wHist, hBase - hOff0); //topleft
+      vertex(wHist + w0, hBase - hOff1);
+      vertex(wHist + w0 + w1, hBase - hOff2);
+      vertex(wHist + w0 + w1 + w2, hBase - hOff3); //topright
+      vertex(wHist + w0 + w1 + w2, hBase + hOff3); //botright
+      vertex(wHist + w0 + w1, hBase + hOff2);
+      vertex(wHist + w0, hBase + hOff1);
+      vertex(wHist, hBase + hOff0); //botleft
       endShape();
-      wHist += w0 +w1+w2;
+      wHist += w0 + w1 + w2;
+
     }
-    if (this.k < this.parentRay.length-1){
 
-      for (let i = 0; i < this.parentRay.length - 1; i++) {
 
-        let v0 = this.parentRay[i].children[0];
-        let v1 = this.parentRay[i];
-        let v2 = this.parentRay[i].children[1];
-        let v3 = this.parentRay[i + 1].children[0];
 
-        let aDiff0 = v1.angle - v0.angle;
-        let aDiff1 = v2.angle - v1.angle;
-        let aDiff2 = v3.angle - v2.angle;
 
-        let hBase = height/2;
-        let hTune = 1;
-        let dist0 = map(v0.collidedRad,0,width,height/2,0);
-        dist0 = constrain(dist0,0,height);
-        let dist1 = map(v1.collidedRad,0,width,height/2,0);
-        dist1 = constrain(dist1,0,height);
-        let dist2 = map(v2.collidedRad,0,width,height/2,0);
-        dist2 = constrain(dist2,0,height);
-        let dist3 = map(v3.collidedRad,0,width,height/2,0);
-        dist3 = constrain(dist3,0,height);
+  //connect last to first
+  // let v1 = this.parentRay[0];
+  // let v2 = this.parentRay[this.parentRay.length - 1];
+  // let aDiff = TWO_PI - (v2.angle - v1.angle);
+  // // print("heyy" + aDiff);
+  // let w = map(aDiff, 0, TWO_PI, 0, width);
+  // rectMode(CORNERS);
+  // fill(0);
+  // rect(wHist, 50, wHist + w, 100);
+  // print("angleDif: w" + angleDiffNet);
 
-        let hOff0 = (dist0*v1.collidedH)*hTune;
-        let hOff1 = (dist1*v1.collidedH)*hTune;
-        let hOff2 = (dist2*v1.collidedH)*hTune;
-        let hOff3 = (dist3*v1.collidedH)*hTune;
 
-        let opacityFill = map((v1.collidedRad),0,width,255,0);
-        let w0 = map(aDiff0, 0, TWO_PI, 0, width);
-        let w1 = map(aDiff1, 0, TWO_PI, 0, width);
-        let w2 = map(aDiff2, 0, TWO_PI, 0, width);
-        // rect(wHist, hBase+hOff1, wHist + w, hBase-hOff1);
-        fill(v1.collidedR,v1.collidedG,v1.collidedB,opacityFill)
-        beginShape();
-        vertex(wHist,hBase-hOff0); //topleft
-        vertex(wHist+w0,hBase-hOff1);
-        vertex(wHist+w0+w1,hBase-hOff2);
-        vertex(wHist+w0+w1+w2,hBase-hOff3); //topright
-        vertex(wHist+w0+w1+w2,hBase+hOff3); //botright
-        vertex(wHist+w0+w1,hBase+hOff2);
-        vertex(wHist+w0,hBase+hOff1);
-        vertex(wHist,hBase+hOff0); //botleft
-        endShape();
-        wHist += w0 +w1+w2;
+}
+updateRays() {
+  /* set every parentRay (ray with children) target to a unique vertex in the scene,
+  set its origin to the light's origin, update the ray */
+  let k = 0;
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[i].vertNumber; j++) {
+      this.parentRay[k].x = this.x;
+      this.parentRay[k].y = this.y;
+      this.parentRay[k].targetX = shape[i].vertX[j];
+      this.parentRay[k].targetY = shape[i].vertY[j];
+      this.parentRay[k].update();
+      k++;
+    }
+  }
+}
+input() {
+  if (keyIsDown(this.upKey)) {
+    this.velX += cos(this.angle) * this.velIncrement;
+    this.velY += sin(this.angle) * this.velIncrement;
+  }
+  if (keyIsDown(this.downKey)) {
+    this.velX += cos(this.angle + PI) * this.velIncrement;
+    this.velY += sin(this.angle + PI) * this.velIncrement;
+  }
+  if (keyIsDown(this.leftKey)) {
+    this.velX += cos(this.angle - HALF_PI) * this.velIncrement;
+    this.velY += sin(this.angle - HALF_PI) * this.velIncrement;
+  }
+  if (keyIsDown(this.rightKey)) {
+    this.velX += cos(this.angle + HALF_PI) * this.velIncrement;
+    this.velY += sin(this.angle + HALF_PI) * this.velIncrement;
+  }
+
+}
+changeAngle() {
+  if (keyIsDown(LEFT_ARROW)) {
+    this.angle -= this.angularIncrement;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    this.angle += this.angularIncrement;
+  }
+  if (this.angle < -PI) {
+    this.angle += TWO_PI;
+  }
+  if (this.angle > PI) {
+    this.angle -= TWO_PI;
+  }
+  // this.angle = atan2(mouseY - this.y, mouseX - this.x);
+  // this.angle = map(mouseX,0,width,0,TWO_PI);
+}
+changePos() {
+  this.velX = this.velX * this.drag;
+  this.velY = this.velY * this.drag;
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+display() {
+  stroke(255);
+  fill(51);
+  strokeWeight(4);
+  ellipse(this.x, this.y, this.radius * 2);
+  line(this.x, this.y, this.x + (cos(this.angle) * this.radius), this.y + (sin(this.angle) * this.radius));
+}
+selectionSort() {
+  /*selectionSort parentRay array by their angles... It basically cycles through
+  the array and finds the smallest value and puts it at the start of the array, then
+  itterates through the array again but starts at index 1, finds the smallest value
+  and puts it at index one, now it starts at index 2....
+   (this really should be at least an insertion-sort algorithim because selection-sort
+   always takes the same amount of calculations even if the array is already sorted,
+    but it is slightly harder to implement) */
+  for (let i = 0; i < this.parentRay.length; i++) {
+    let smallestValue = Infinity;
+    let smallestValueIndex;
+    for (let j = i; j < this.parentRay.length; j++) {
+      //cycle through this.parentRay[], find index of smallest value
+      if (this.parentRay[j].angle < smallestValue) {
+        smallestValueIndex = j;
+        smallestValue = this.parentRay[j].angle;
       }
-    } else{
-      this.k = 0;
-    }
-    //connect last to first
-    // let v1 = this.parentRay[0];
-    // let v2 = this.parentRay[this.parentRay.length - 1];
-    // let aDiff = TWO_PI - (v2.angle - v1.angle);
-    // // print("heyy" + aDiff);
-    // let w = map(aDiff, 0, TWO_PI, 0, width);
-    // rectMode(CORNERS);
-    // fill(0);
-    // rect(wHist, 50, wHist + w, 100);
-    // print("angleDif: w" + angleDiffNet);
-
-
-  }
-  updateRays() {
-    /* set every parentRay (ray with children) target to a unique vertex in the scene,
-    set its origin to the light's origin, update the ray */
-    let k = 0;
-    for (let i = 0; i < shape.length; i++) {
-      for (let j = 0; j < shape[i].vertNumber; j++) {
-        this.parentRay[k].x = this.x;
-        this.parentRay[k].y = this.y;
-        this.parentRay[k].targetX = shape[i].vertX[j];
-        this.parentRay[k].targetY = shape[i].vertY[j];
-        this.parentRay[k].update();
-        k++;
+      //once at end of the arthis.parentRay, swap this.parentRay index i with smallest this.parentRay...
+      if (j === this.parentRay.length - 1) {
+        let parentRayStore = this.parentRay[i];
+        this.parentRay[i] = this.parentRay[smallestValueIndex];
+        this.parentRay[smallestValueIndex] = parentRayStore;
       }
+      //then increment i and repeat until parentRay[] is sorted...
     }
   }
-  input() {
-    if (keyIsDown(this.upKey)) {
-      this.velX += cos(this.angle) * this.velIncrement;
-      this.velY += sin(this.angle) * this.velIncrement;
-    }
-    if (keyIsDown(this.downKey)) {
-      this.velX += cos(this.angle + PI) * this.velIncrement;
-      this.velY += sin(this.angle + PI) * this.velIncrement;
-    }
-    if (keyIsDown(this.leftKey)) {
-      this.velX += cos(this.angle - HALF_PI) * this.velIncrement;
-      this.velY += sin(this.angle - HALF_PI) * this.velIncrement;
-    }
-    if (keyIsDown(this.rightKey)) {
-      this.velX += cos(this.angle + HALF_PI) * this.velIncrement;
-      this.velY += sin(this.angle + HALF_PI) * this.velIncrement;
-    }
-
-  }
-  changeAngle() {
-    if (keyIsDown(LEFT_ARROW)) {
-      this.angle -= this.angularIncrement;
-    }
-    if (keyIsDown(RIGHT_ARROW)) {
-      this.angle += this.angularIncrement;
-    }
-    if (this.angle < -PI){
-      this.angle += TWO_PI;
-    }
-    if (this.angle > PI){
-      this.angle -= TWO_PI;
-    }
-    // this.angle = atan2(mouseY - this.y, mouseX - this.x);
-    // this.angle = map(mouseX,0,width,0,TWO_PI);
-  }
-  changePos() {
-    this.velX = this.velX * this.drag;
-    this.velY = this.velY * this.drag;
-
-    this.x += this.velX;
-    this.y += this.velY;
-  }
-  display() {
-    stroke(255);
-    fill(51);
-    strokeWeight(4);
-    ellipse(this.x, this.y, this.radius * 2);
-    line(this.x, this.y, this.x + (cos(this.angle) * this.radius), this.y + (sin(this.angle) * this.radius));
-  }
-  selectionSort() {
-    /*selectionSort parentRay array by their angles... It basically cycles through
-    the array and finds the smallest value and puts it at the start of the array, then
-    itterates through the array again but starts at index 1, finds the smallest value
-    and puts it at index one, now it starts at index 2....
-     (this really should be at least an insertion-sort algorithim because selection-sort
-     always takes the same amount of calculations even if the array is already sorted,
-      but it is slightly harder to implement) */
-    for (let i = 0; i < this.parentRay.length; i++) {
-      let smallestValue = Infinity;
-      let smallestValueIndex;
-      for (let j = i; j < this.parentRay.length; j++) {
-        //cycle through this.parentRay[], find index of smallest value
-        if (this.parentRay[j].angle < smallestValue) {
-          smallestValueIndex = j;
-          smallestValue = this.parentRay[j].angle;
-        }
-        //once at end of the arthis.parentRay, swap this.parentRay index i with smallest this.parentRay...
-        if (j === this.parentRay.length - 1) {
-          let parentRayStore = this.parentRay[i];
-          this.parentRay[i] = this.parentRay[smallestValueIndex];
-          this.parentRay[smallestValueIndex] = parentRayStore;
-        }
-        //then increment i and repeat until parentRay[] is sorted...
-      }
-    }
-  }
+}
 
 }
