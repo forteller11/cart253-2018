f431241d851ff5e6ec075e483967cd064b0c7003
forteller11 <42951087+forteller11@users.noreply.github.com>
2018-11-13 02:02:32 -0500

    ex8 playing around with functions, creating interesting sound bytes

--- excercises/exercise8/js/script2.js
+++ excercises/exercise8/js/script2.js
@@ -1,8 +1,8 @@
 let t = 0;
 const frequency = 440;
-const angularFrequency = frequency * 2 * Math.PI;
+const angularFrequency = 2 * Math.PI;
 const sampleRate = 44100;
-const sampleNumber = sampleRate * 2;
+const sampleNumber = sampleRate * 16;
 let audioContext = new AudioContext();
 let myBuffer = audioContext.createBuffer(1, sampleNumber, sampleRate);
 let myArray = myBuffer.getChannelData(0);
@@ -11,10 +11,17 @@ for (let i = 0 ; i < sampleNumber ; i++) {
 }
 
 function generateSample(i) {
-  t+= 0.0001;
+  t+= 1/sampleNumber;
   let sampleTime = i / sampleRate;
   let sampleAngle = sampleTime * angularFrequency;
-  return Math.sin(sampleAngle)*Math.tan(i/10000);
+  // print(sampleAngle);
+  let f0 = Math.sin(sampleAngle * 20);
+  let f1 = Math.sin(sampleAngle *7);
+  let f2 = Math.cos(Math.pow(sampleAngle*.8,f1));
+  let f3 = Math.sin(f0*10);
+  // f3 = constrain(f3,0,1);
+  let f4 = Math.cos(f1*30);
+  return (f3+(f4*t)+f2);
 }
 
 let src = audioContext.createBufferSource();
@@ -23,7 +30,7 @@ src.connect(audioContext.destination);
 src.start();
 
 let iIndex = 0;
-let iInc = 20;
+let iInc = (1.86*sampleNumber)/(sampleRate);
 function setup(){
   createCanvas(1000,1000);
   background(0);
@@ -36,11 +43,12 @@ function draw(){
 
   for (let i = iIndex ; i*iInc < sampleNumber ; i++) {
 
-    let yy1 = generateSample(i*iInc)*height/4 + height/2;
-    let yy2 = generateSample(i*iInc+iInc)*height/4 + height/2;
+    let yy1 = generateSample(i*iInc)*height/30 + height/2;
+    let yy2 = generateSample(i*iInc+iInc)*height/30 + height/2;
     line(i-iIndex,yy1,i+1-iIndex,yy2);
-
+// print(yy1);
     // point(i-iIndex,(yy*height/2)+(height/2));
   }
-iIndex += 10;
+  // print(iInc/sampleNumber)
+iIndex += (iInc);
 }
